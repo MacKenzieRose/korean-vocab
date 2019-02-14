0dd3da018f4ef9cd9883cc08ae3cc4c2a1df212a
MacKenzie Bulkowski-Rose <mackenzie.bulkowski.rose@gmail.com>
2019-02-14 12:56:58 -0500

    Re-import content for public repository

---
+++ constants.js
@@ -0,0 +1,67 @@
+const FLASHCARD_DECKS = [{
+  title: 'Korean Alphabet',
+  words: {
+    'ㄱ': 'giyeok',
+    'ㄴ': 'nieun',
+    'ㄷ': 'digeut',
+    'ㄹ': 'rieul',
+    'ㅁ': 'mieum',
+    'ㅂ': 'bieup',
+    'ㅅ': 'siot',
+    'ㅇ': 'ieung',
+    'ㅈ': 'jieut',
+    'ㅊ': 'chieut',
+    'ㅋ': 'kieuk',
+    'ㅌ': 'tieut',
+    'ㅍ': 'pieup',
+    'ㅎ': 'hieut',
+    'ㄲ': 'ssanggiyeok',
+    'ㄸ': 'ssangdigeut',
+    'ㅃ': 'ssangbieup',
+    'ㅆ': 'ssangsiot',
+    'ㅉ': 'ssangjieut',
+    'ㅏ': 'a',
+    'ㅑ': 'ya',
+    'ㅓ': 'eo',
+    'ㅕ': 'yeo',
+    'ㅗ': 'o',
+    'ㅛ': 'yo',
+    'ㅜ': 'u',
+    'ㅠ': 'yu',
+    'ㅡ': 'eu',
+    'ㅣ': 'i',
+    'ㅐ': 'ae',
+    'ㅒ': 'yae',
+    'ㅔ': 'e',
+    'ㅖ': 'ye',
+    'ㅘ': 'wa',
+    'ㅙ': 'wae',
+    'ㅚ': 'oe',
+    'ㅝ': 'wo',
+    'ㅞ': 'we',
+    'ㅟ': 'wi',
+    'ㅢ': 'ui'
+  }
+}, {
+  title: 'Basic Korean Words',
+  words: {
+    '여자': 'woman',
+    '남자': 'man',
+    '사람': 'person',
+    '나무': 'tree',
+    '호수': 'lake',
+    '구름': 'cloud',
+    '땅': 'ground'
+  }
+}, {
+  title: 'Foods in Korean',
+  words: {
+    '사과': 'apple',
+    '빵': 'bread',
+    '치즈': 'cheese',
+    '우유': 'milk',
+    '커피': 'coffee',
+    '바나나': 'banana',
+    '복숭아': 'peach'
+  }
+}];
