04d0f4bea7226c8302ba6a7bf5bf40e27e380688
MacKenzie Bulkowski-Rose <mackenzie.bulkowski.rose@gmail.com>
2019-02-14 13:48:16 -0500

    Add verbs vocabulary

--- constants.js
+++ constants.js
@@ -43,6 +43,34 @@ const FLASHCARD_DECKS = [{
     'ㅢ': 'ui'
   }
 }, {
+  title: 'Korean Verbs',
+  words: {
+    '가다': 'to go',
+    'conjugate 가다': '가요',
+    '자다': 'to sleep',
+    'conjugate 자다': '자요',
+    '오다': 'to come',
+    'conjugate 오다': '와요',
+    '보다': 'to see/watch',
+    'conjugate 보다': '봐요',
+    '먹다': 'to eat',
+    'conjugate 먹다': '먹어요',
+    '마시다': 'to drink',
+    'conjugate 마시다': '마셔요',
+    '공부하다': 'to study',
+    'conjugate 공부하다': '공부해요',
+    '운동하다': 'to do exercise',
+    'conjugate 운동하다': '운동해요',
+    '전화하다': 'to call',
+    'conjugate 전화하다': '전화해요',
+    '일하다': 'to work',
+    'conjugate 일하다': '일해요',
+    '이야기하다': 'to talk',
+    'conjugate 이야기하다': '이야기해요',
+    '쇼핑하다': 'to shop',
+    'conjugate 쇼핑하다': '쇼핑해요',
+  }
+}, {
   title: 'Basic Korean Words',
   words: {
     '여자': 'woman',
